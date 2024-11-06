51ef2ff1cc5498de41432b215900a8e893d86ed1
apo-star <apolle.world@gmail.com>
2024-11-06 06:43:20 -0800

    initial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,5 +2,5 @@
 'use client'
 
 export default function HomePage() {
-  return <div className="h-screen w-screen select-none text-[100px]">venusdev@eclipso.email,,,,,,Please send mail to this email address.</div>
+  return <div className="h-screen w-screen select-none "></div>
 }
